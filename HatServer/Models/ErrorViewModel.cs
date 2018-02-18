8500cfaa18a9890af4416a37f7fa98e9ef41939d
Grigory Zhadko <gregzhadko@gmail.com>
2018-02-18 22:24:07 +0100

    Basic asp.net core web application with users

---
+++ HatServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HatServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
