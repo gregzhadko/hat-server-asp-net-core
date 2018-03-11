bf8b1bdc32b099fae7fd641bb57443b678d4e18f
Grigory Zhadko <gregzhadko@gmail.com>
2018-03-11 16:25:14 +0100

    Added Console project for testing purposes + Console utilities for cute information representation in Console

---
+++ ConsoleMigration/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Alba.CsConsoleFormat;
+using Alba.CsConsoleFormat.Fluent;
+using HatServer.Old;
+using Utilities;
+
+namespace ConsoleMigration
+{
+    public class Program
+    {
+        private static readonly LineThickness StrokeHeader = new LineThickness(LineWidth.None);
+        private static readonly LineThickness StrokeRight = new LineThickness(LineWidth.None, LineWidth.None, LineWidth.Single, LineWidth.None);
+
+        public static void Main(string[] args)
+        {
+            Run();
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+
+        private static void Run()
+        {
+            var oldService = new OldService();
+            var packs = OldService.GetAllPacksAsync(null).Result;
+          
+        }
+    }
+}
---
+++ Utilities/ConsoleUtilities.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using Alba.CsConsoleFormat;
+using Alba.CsConsoleFormat.Fluent;
+
+namespace Utilities
+{
+    public static class ConsoleUtilities
+    {
+        public static void WriteInfo(string firstColumn, params string[] phrases)
+        {
+            var document = new Document
+            {
+                Background = ConsoleColor.Black,
+                Color = ConsoleColor.Gray,
+                Children =
+                {
+                    new Grid
+                    {
+                        Stroke = new LineThickness(0, 0),
+                        StrokeColor = ConsoleColor.DarkGray,
+                        Columns =
+                        {
+                            new Column {Width = GridLength.Auto, MinWidth = 12},
+                            new Column {Width = GridLength.Auto, MinWidth = 25},
+                            phrases.Select(p => new Column {Width = GridLength.Star(1)})
+                        },
+                        Children =
+                        {
+                            new Cell
+                            {
+                                Stroke = new LineThickness(0, 0),
+                                Children = {DateTime.Now.ToString("hh:mm:ss")}
+                            },
+                            new Cell
+                            {
+                                Stroke = new LineThickness(0, 0),
+                                Color = ConsoleColor.Green,
+                                Children = {firstColumn}
+                            },
+                            phrases.Select(phrase => new[]
+                            {
+
+                                new Cell {Children = {phrase}, Stroke = new LineThickness(0, 0)}
+                            })
+                        }
+                    }
+                }
+            };
+
+            ConsoleRenderer.RenderDocument(document);
+        }
+
+        public static void WriteException(Exception exception, string message = "")
+        {
+            Console.WriteLine($"{message}\n{exception}".Red());
+        }
+    }
+}
