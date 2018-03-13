a874d2ea3e2915c9bf859efe1b0c9e374b363850
Grigory Zhadko <gregzhadko@gmail.com>
2018-03-13 15:52:15 +0100

    Added loading and translation for the simple words

--- ConsoleMigration/Program.cs
+++ ConsoleMigration/Program.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Alba.CsConsoleFormat;
 using HatServer.Old;
+using Utilities;
+using YandexTranslateCSharpSdk;
 
 namespace ConsoleMigration
 {
@@ -8,19 +14,48 @@ namespace ConsoleMigration
     {
         private static readonly LineThickness StrokeHeader = new LineThickness(LineWidth.None);
         private static readonly LineThickness StrokeRight = new LineThickness(LineWidth.None, LineWidth.None, LineWidth.Single, LineWidth.None);
+        static YandexTranslateSdk _translatorWrapper;
 
         public static void Main(string[] args)
         {
+            _translatorWrapper = new YandexTranslateSdk();
+            _translatorWrapper.ApiKey = File.ReadLines("Settings.txt").ToArray()[1];
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             Run();
             Console.WriteLine("Press any key...");
             Console.ReadKey();
         }
 
+
         private static void Run()
         {
             var oldService = new OldService();
-            var packs = OldService.GetAllPacksAsync(null).Result;
-          
+            //var packs = OldService.GetAllPacksAsync(null).Result;
+
+            //packs.ForEach(p => Console.WriteLine($"{p.Id} {p.Name}"));
+
+            var pack = OldService.GetPack(13).Result;
+            var finalList = new ConcurrentBag<(string, string)>();
+
+            Parallel.ForEach(pack.Phrases, phrase =>
+            {
+                var translated = Translate(phrase.Phrase);
+                finalList.Add((phrase.Phrase, translated));
+                ConsoleUtilities.WriteInfo("Translated", phrase.Phrase, translated);
+            });
+
+            //const string fileName = @"C:\SimpleWords.txt";
+            //File.Create(fileName);
+            //File.WriteAllLines(fileName, finalList.Select(i => String.Format("{0,-30}{1,10}", i.Item1, i.Item2)));
+
+            finalList.ToList().ForEach(i => Console.WriteLine(String.Format("{0,-30}{1,10}", i.Item1, i.Item2)));
+        }
+
+        private static string Translate(string text)
+        {
+            return _translatorWrapper.TranslateText(text, "ru-en").Result;
         }
     }
 }
