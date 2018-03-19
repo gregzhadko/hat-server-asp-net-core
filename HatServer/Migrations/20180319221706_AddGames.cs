547b8acaba74aaf50e51131b748bc6198051f40d
Grigory Zhadko <gregzhadko@gmail.com>
2018-03-19 23:18:02 +0100

    Added Games

---
+++ HatServer/Migrations/20180319221706_AddGames.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HatServer.Migrations
+{
+    public partial class AddGames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GameId",
+                table: "Teams",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Games_GameUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "GameUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teams_GameId",
+                table: "Teams",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_UserId",
+                table: "Games",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Teams_Games_GameId",
+                table: "Teams",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Teams_Games_GameId",
+                table: "Teams");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Teams_GameId",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "GameId",
+                table: "Teams");
+        }
+    }
+}
--- HatServer/Models/Team.cs
+++ HatServer/Models/Team.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace HatServer.Models
 {
@@ -10,8 +12,9 @@ namespace HatServer.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public int GameId { get; set; }
+        [ForeignKey("GameId")]
         public Game Game { get; set; }
+        public int GameId { get; set; }
 
         public List<Player> Players { get; set; }
     }
