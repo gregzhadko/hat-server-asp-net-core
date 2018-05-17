be16fe8cfcc27bc29e7098575e4d17c3573c44c3
Grigory Zhadko <gregzhadko@gmail.com>
2018-05-17 17:17:23 +0200

    Added migration for required fields of packs

---
+++ HatServer/Migrations/20180517104039_RequiredFieldsForPacks.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HatServer.Migrations
+{
+    public partial class RequiredFieldsForPacks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "PhraseItems",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Packs",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Language",
+                table: "Packs",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "PhraseItems");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Packs",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Language",
+                table: "Packs",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
