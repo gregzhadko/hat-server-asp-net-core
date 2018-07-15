f3d445fa3160f3bdc8786d0a3271ab84eb100f69
Grigory Zhadko <gregzhadko@gmail.com>
2018-07-15 20:28:57 +0200

    Removing of prod data from filler db context

--- Model/Entities/Stage.cs
+++ Model/Entities/Stage.cs
@@ -1,20 +1,21 @@
-﻿//using System.Collections.Generic;
-//using System.ComponentModel.DataAnnotations;
-//using System.ComponentModel.DataAnnotations.Schema;
-//using System.Runtime.InteropServices;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using JetBrains.Annotations;
 
-//namespace HatServer.Models
-//{
-//    public class Stage
-//    {
-//        public int Id { get; set; }
-//        public int Number { get; set; }
-//        public int Time { get; set; }
+namespace Model.Entities
+{
+    [UsedImplicitly]
+    public sealed class Stage
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public int Time { get; set; }
 
-//        [ForeignKey("GameId")]
-//        public Game Game { get; set; }
-//        public int GameId { get; set; }
+        [ForeignKey("GameId")]
+        public Game Game { get; set; }
 
-//        public List<Round> Rounds { get; set; }
-//    }
-//}
+        public int GameId { get; set; }
+
+        public List<Round> Rounds { get; set; }
+    }
+}
