a0b283e73b82400d11f676128d908476e0ca9918
Grigory Zhadko <gregzhadko@gmail.com>
2018-07-15 20:25:42 +0200

    Additional test for PackRespository + Compare methods for PhraseItems, ReviewStates and nullable DateTimes

--- Model/Entities/ReviewState.cs
+++ Model/Entities/ReviewState.cs
@@ -1,11 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using JetBrains.Annotations;
 using Utilities;
 
 namespace Model.Entities
 {
-    public sealed class ReviewState : ICloneable<ReviewState>
+    public sealed class ReviewState : ICloneable<ReviewState>, IEquatable<ReviewState>
     {
         public int Id { get; set; }
 
@@ -39,5 +40,49 @@ namespace Model.Entities
 
         [NotNull]
         public override string ToString() => $"{User.UserName}: {State}";
+
+        public bool Equals([CanBeNull] ReviewState other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return Id == other.Id && PhraseItemId == other.PhraseItemId && string.Equals(UserId, other.UserId) &&
+                   State == other.State && string.Equals(Comment, other.Comment);
+        }
+
+        public override bool Equals([CanBeNull] object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            return obj is ReviewState state && Equals(state);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Id;
+                hashCode = (hashCode * 397) ^ PhraseItemId;
+                hashCode = (hashCode * 397) ^ (UserId != null ? UserId.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (int)State;
+                hashCode = (hashCode * 397) ^ (Comment != null ? Comment.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
     }
 }
