dba91aa491bbfe9a74200733bec6c512a8ad4586
Grigory Zhadko <gregzhadko@gmail.com>
2018-07-19 23:56:01 +0200

    Useless user was removed. .gitignore was updated

--- HatServer/Controllers/Api/GamePacksController.cs
+++ HatServer/Controllers/Api/GamePacksController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HatServer.Controllers.Api
 {
-    [Authorize]
+    //[Authorize]
     [Route("api/[controller]")]
     public sealed class GamePacksController : Controller
     {
--- HatServer/Data/DbInitializer.cs
+++ HatServer/Data/DbInitializer.cs
@@ -49,12 +49,12 @@ namespace HatServer.Data
             var fomin = new ServerUser {UserName = "fomin"};
             var sivykh = new ServerUser {UserName = "sivykh"};
             var tatarintsev = new ServerUser {UserName = "tatarintsev"};
-            var stat = new ServerUser {UserName = "stat"};
+            //var stat = new ServerUser {UserName = "stat"};
             _userManager.CreateAsync(zhadko, _configuration["zhadko"]).Wait();
             _userManager.CreateAsync(fomin, _configuration["fomin"]).Wait();
             _userManager.CreateAsync(sivykh, _configuration["sivykh"]).Wait();
             _userManager.CreateAsync(tatarintsev, _configuration["tatarintsev"]).Wait();
-            _userManager.CreateAsync(stat, _configuration["stat"]).Wait();
+            //_userManager.CreateAsync(stat, _configuration["stat"]).Wait();
         }
 
         private void SeedPacks()
@@ -62,6 +62,7 @@ namespace HatServer.Data
             var users = _userManager.Users.ToList();
 
             var packs = MongoServiceClient.GetAllPacksAsync(users).GetAwaiter().GetResult();
+
 
             _context.Packs.AddRange(packs);
         }
