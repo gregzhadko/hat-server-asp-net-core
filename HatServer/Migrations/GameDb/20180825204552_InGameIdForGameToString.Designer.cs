af8f7202023b450bed3b0c148593ed9274ebd538
Grigory Zhadko <gregzhadko@gmail.com>
2018-08-25 22:47:22 +0200

    Replacement of InGameId property with string instead of GUID

--- HatServer/Migrations/GameDb/20180825204552_InGameIdForGameToString.Designer.cs
+++ HatServer/Migrations/GameDb/20180825204552_InGameIdForGameToString.Designer.cs
@@ -4,14 +4,16 @@ using HatServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HatServer.Migrations.GameDb
 {
     [DbContext(typeof(GameDbContext))]
-    partial class GameDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180825204552_InGameIdForGameToString")]
+    partial class InGameIdForGameToString
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
